f4aa24cd554600a75b62057459c2b1641bbdaf5d
darialiit <solomkina.daria@gmail.com>
2023-07-05 08:50:22 -0300

    delete old code

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Ask the player for the name and update the name on the page
+// ask the player's name and update the name on the page
 /*let playerName = prompt("what is your name?")
 const playerNameJ = document.querySelector("#player-score");
 playerNameJ.textContent = playerName + ":"; 
@@ -31,7 +31,7 @@ function showComputerChoice(a){
 };
 
 let computerRoundChoice;
-// Generate Computer choice
+// generate Computer choice
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
     let index = Math.floor(Math.random()* 3);
@@ -59,12 +59,10 @@ function compareChoices(player, computer) {
     return {scorePlayer, scoreComputer};
 };
 
-// Update the Total Score
-// and Show the current score on the page
+// update the Total Score
+// and show the current score on the page
 let correntPlayerScore = 0;
 let correntComputerScore = 0;
-
-// XXXXXXXXXXXXXXXXXXX HERE
 const playerScoreN = document.querySelector("#player-score-n");
 const computerScoreN = document.querySelector("#computer-score-n");
 
@@ -92,78 +90,15 @@ function roundStatusUpdate(scoreP, scoreC) {
 choices.forEach((column) => {
     column.addEventListener("click", () => {
         playerChoice = column.innerText; // returns the name of the Choice
-        getComputerChoice(); // try to regenerate the choice
-        console.log(computerRoundChoice);
+        getComputerChoice();
         showPlayerChoice(playerChoice);
         showComputerChoice(computerRoundChoice);
         roundResult = compareChoices(playerChoice, computerRoundChoice);
-        console.log(updateCount(roundResult.scorePlayer, roundResult.scoreComputer));
+        updateCount(roundResult.scorePlayer, roundResult.scoreComputer);
         roundStatusUpdate(roundResult.scorePlayer, roundResult.scoreComputer);
     });
 });
 
+// add a stop when one of the scores reaches 5 points
 
 
-
-
-
-
-
-/* 
-let result;
-let scorePlayer;
-let scoreComputer;
-
-// Function to play 1 round and return the number based on the win
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        scorePlayer = 0;
-        scoreComputer = 0;
-    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") || ((playerSelection == "paper" && computerSelection == "scissors"))) {
-        scorePlayer = 0;
-        scoreComputer = 1;
-    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        scorePlayer = 1;
-        scoreComputer = 0;
-    } else {
-        scorePlayer = 0;
-        scoreComputer = 0;
-    }
-return {scorePlayer, scoreComputer, result};
-}
-
-
-// Function for the game of 5 rounds (loop)
-let countWinsPlayer = 0;
-let countWinsComputer = 0;
-
-for (let i = 0; i < 5; i++) {
-    let message = prompt("Type: Rock, Paper or Scissors");
-    let playerSelection = message.toLocaleLowerCase();
-    console.log("Players choice: " + playerSelection);
-    const computerSelection = getComputerChoice();
-    console.log("Computer`s choice: " + computerSelection);
-    let scorePlayerN = Number(playRound(playerSelection, computerSelection).scorePlayer);
-    let scoreComputerN = Number(playRound(playerSelection, computerSelection).scoreComputer);
-
-    if (scorePlayerN == 1) {
-        countWinsPlayer = countWinsPlayer + 1;
-    } else if (scoreComputerN == 1) {
-        countWinsComputer = countWinsComputer + 1;
-    }
-
-    console.log("Players score: " + countWinsPlayer);
-    console.log("Conputer score: " + countWinsComputer);
-  }
-
-// Alert the winner
-
-if (countWinsPlayer > countWinsComputer) {
-    alert("You have won the game. Congrats!")
-} else if (countWinsComputer > countWinsPlayer) {
-    alert("Sorry, you lost :(")
-} else {
-    alert("It is a draw, play one more time to determine the winner")
-}
-
-*/
